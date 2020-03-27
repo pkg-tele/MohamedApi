f7879d4967d182dc4ba6c02c8b49773a021afd0e
pkg-tele <62763496+pkg-tele@users.noreply.github.com>
2020-03-27 20:23:34 +0300

    Add files via upload

---
+++ Fastrun.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+cd $HOME/MohamedApi
+rm -rf $HOME/.telegram-cli
+MohamedApiins() {
+sudo chmod +x tg
+chmod +x MohamedApi
+chmod +x Mo
+./ts
+}
+if [ "$1" = "ins" ]; then
+MohamedApiins
+fi
+chmod +x Run.sh
+lua Run.lua
